ea0ea6c138b0f2159a9675ca3057751826cdae3e
muneebahmedayub <muneebahmedayub01@gmail.com>
2023-08-20 10:00:01 +0500

    test expo updates

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello these are changes from expo updates</Text>
       <StatusBar style="auto" />
     </View>
   );
